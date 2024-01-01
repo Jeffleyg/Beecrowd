b9c8e4f195c89c38da1db8ace0868854eec32c5c
Jeffleyg <jefleygarcon007@gmail.com>
2024-01-01 13:20:54 -0300

    Meu commit

--- src/application/SortedMult.java
+++ src/application/SortedMult.java
@@ -1,10 +1,27 @@
 package application;
 
+import java.util.Scanner;
+
 public class SortedMult {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+        Scanner scanner = new Scanner(System.in);
+        int A, B, C;
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        C = scanner.nextInt();
+        
+        int menor, maior, meio;
+        
+        menor = Math.min(Math.min(A, B), C);
+        maior = Math.max(Math.max(A, B), C);
+        meio = A + B + C - menor - maior;
+        
+        System.out.println(menor);
+        System.out.println(meio);
+        System.out.println(maior);
+        System.out.println();
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
 	}
-
 }
